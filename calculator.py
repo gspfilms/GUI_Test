84f2b24a4203efbbe0dbe88cd3b81e85cd24d11a
gspfilms <50781267+gspfilms@users.noreply.github.com>
2020-01-14 20:12:27 -0700

    updated GUI.py project

--- calculator.py
+++ calculator.py
@@ -6,21 +6,25 @@ root.title("Simple Calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
 
-def button_add():
-	return
+def button_click(number):
+	current = e.get()
+	e.insert(0, str(current) + str(number)
 
 #Define buttons
 
-button_1 = Button(root, text="1", padx=20, pady=10, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+button_1 = Button(root, text="1", padx=40, pady=20, command=Lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=Lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=Lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=Lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=Lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=Lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=Lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=Lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=Lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, command=Lambda: button_click(0))
+button_click = Button(root, text="+", padx=39, pady=20, command=Lambda: button_click())
+button_equal = Button(root, text="=", padx=91, pady=20, command=Lambda: button_click())
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=Lambda: button_click())
 
 #Put the buttons on screen
 
@@ -37,5 +41,8 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+Lambda: button_click()).grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
 
 root.mainloop()
