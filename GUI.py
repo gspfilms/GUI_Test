5adab1c660d9a749f9e3ce0569d256146bd58771
gspfilms <50781267+gspfilms@users.noreply.github.com>
2020-01-14 21:04:05 -0700

    added some functionality to Rick and Morty app

--- GUI.py
+++ GUI.py
@@ -1,5 +1,46 @@
 from tkinter import *
 
+#keydown function
+def click():
+    entered_text=textentry.get()
+    #entered_text=raw_entry.lower
+    output.delete(0.0, END)
+    try:
+        definition = my_compdictionary[entered_text]
+    except:
+        definition = "sorry I haven't programmed that character yet, please try again. *Try all lowercase names"
+    output.insert(END, definition)
 
 
-root.mainloop()
+#Main
+window = Tk()
+window.title("Rick and Morty Maaaaaaaaan")
+window.configure(background="black")
+
+#My photo
+photo1 = PhotoImage(file="me.gif")
+Label (window, image=photo1, bg="black") .grid(row=0, column=0, sticky=E)
+
+#create the label
+Label (window, text="Type in your favorite character for their catchphrase...", bg="black", fg="white", font="none 12 bold") .grid(row=2, column=0, sticky=W)
+
+#creates text entry box
+textentry = Entry(window, width=20, bg="white")
+textentry.grid(row=3, column=0, sticky=W)
+
+# add a submit button
+Button(window, text="SUBMIT", width=6, command=click) .grid(row=4, column=0, sticky=W)
+
+#create another label
+Label (window, text="\nCatchphrase:", bg="black", fg="white", font="none 12 bold") .grid(row=5, column=0, sticky=W)
+
+#create a text box
+output = Text(window, width=75, height=6, wrap=WORD, background="white")
+output.grid(row=6, column=0, columnspan=2, sticky=W)
+
+#the dictionary
+my_compdictionary = {
+    'rick': 'Wubbalubbadubdub!!!', 'tiny rick': "Yo, I'm tiny rick!!!", "morty": "aw geez", "mr. meeseeks": "OOOwweEE",
+    'jerry': 'I"m lame Jerry', 'bird person': 'I"m bird person', 'mr. poopy butthole': 'I was murdered'
+}
+window.mainloop()
